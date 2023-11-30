049428331fad464606c9129bcf440bd31d156616
rezashabrang <rezashabrang.m@gmail.com>
2023-11-30 21:39:11 +0330

    style: Changing indent size.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import AcademicResume from './academic';
+import React from 'react';
 import JobResume from './job';
 import './App.css';
 import './index.css';
@@ -7,34 +6,8 @@ import './index.css';
 
 
 const App = () => {
-  const [showAcademicResume, setShowAcademicResume] = useState(false);
-  const [showJobResume, setShowJobResume] = useState(true);
-
-  const handleAcademicButtonClick = () => {
-    setShowAcademicResume(true);
-    setShowJobResume(false); // Hide job resume
-  };
-
-  const handleJobButtonClick = () => {
-    setShowJobResume(true);
-    setShowAcademicResume(false); // Hide academic resume
-  };
-
   return (
-    <>
-      {(!showAcademicResume && !showJobResume) && (
-        <div className="intro-container">
-          <div className="intro-content">
-            <button className='neon-button' onClick={handleAcademicButtonClick}>Academic Resume</button>
-            <button className='neon-button' onClick={handleJobButtonClick}>Job Resume</button>
-          </div>
-        </div>
-      )}
-
-      {showAcademicResume && <AcademicResume />}
-      {showJobResume && <JobResume />}
-    </>
-
+    <JobResume />
   );
 };
 
